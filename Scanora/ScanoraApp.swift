d10b61a2e08096a2a7b1fc882f532b1e6c2f1644
portwally <walter.tengler@gmail.com>
2026-02-19 20:59:59 +0000

    Initial Commit

---
+++ Scanora/ScanoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanoraApp.swift
+//  Scanora
+//
+//  Created by Walter Tengler on 19/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ScanoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
